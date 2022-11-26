85bc135c480d8d59a5b07af274d284cfd0b49442
Khanh <khanhlp315@gmail.com>
2022-11-27 02:18:57 +0700

    deploy webgl

--- Assets/Scripts/Utility/CustomPropertiesConstant.cs
+++
@@ -1,7 +0,0 @@
-
-public static class CustomPropertiesConstant
-{
-    public const string SKIN_INDEX = "skinIndex";
-    public const string PLAYER_NAME = "playerName";
-    public const string PLAYER_SKIN = "playerSkin";
-}
